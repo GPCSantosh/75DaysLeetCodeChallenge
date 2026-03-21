041a120a7deff09d16515c1e52efd39538d0bd20
Santosh@CR7 <127501283+GPCSantosh@users.noreply.github.com>
2026-03-21 11:07:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,23 +1,5 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // Arrays.sort(nums);
-        // int count = 0;
-        // for(int i = 0 ; i < nums.length ; i++){
-        //     if(nums[i] == 0){
-        //         count++;
-        //     }
-        // }
-        // int k = 0;
-        // int[] temp = new int[nums.length];
-        // for(int i = count ; i < nums.length ; i++){
-        //     temp[k++] = nums[i];
-        // }
-        // for(int i = 0 ; i < count ; i++){
-        //     temp[k++] = nums[i];
-        // }
-        // for(int i = 0 ; i < nums.length ; i++){
-        //     nums[i] = temp[i];
-        // }
         int k = 0;
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i] != 0){
